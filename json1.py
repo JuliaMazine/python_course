5c9455c5c782eec1d09a4269721dc86e679cd518
Julia Mazine <d.mazine@g.nsu.ru>
2023-03-02 08:19:50 +0700

    вроде исправила!

--- json1.py
+++ json1.py
@@ -1,14 +1,20 @@
 import json
 
 itemList = []
+dictionary = {}
 with open('wikidata_1000.json', 'r', encoding='utf-8') as f:
     for jsonObj in f:
-        itemDict = json.loads(jsonObj)
-        itemList.append(itemDict)
+        itemList.append(json.loads(jsonObj))
 
-with open('res1.json', 'w') as j:
     for item in itemList:
+        key = item["label"]['value']
         try:
-            json.dump({item["label"]['value']: item["description"]['value']}, j, ensure_ascii=False, indent=4)
+            value = item["description"]['value']
         except:
-            json.dump({item["label"]['value']: 'None'}, j, ensure_ascii=False, indent=4)
+            value = 'None'
+        dictionary[key] = value
+
+        
+with open('res1.json', 'w') as j:
+    json.dump(dictionary, j, sort_keys=False, ensure_ascii=False, indent=4, separators=(',', ':'))
+
