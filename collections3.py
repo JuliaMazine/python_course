e3e17b19e62b39197f12a4caa3675966e57dae39
Julia Mazine <d.mazine@g.nsu.ru>
2023-03-17 16:47:21 +0700

    исправила!

--- collections3.py
+++ collections3.py
@@ -1,13 +1,14 @@
 import json
-from collections import Counter as cnt
-counter_dict = {}
+import collections
+cnt = collections.Counter()
 with open('RomeoAndJuliet.json', 'r') as f:
     data = json.load(f)
     for j in range(len(data['acts'])):
         for k in range(len(data['acts'][j]['scenes'])):
             for l in range(len(data['acts'][j]['scenes'][k]['action'])):
-                    if data['acts'][j]['scenes'][k]['action'][l]['character'] not in counter_dict:
-                        counter_dict[data['acts'][j]['scenes'][k]['action'][l]['character']] = 0
-                    else:
-                        counter_dict[data['acts'][j]['scenes'][k]['action'][l]['character']] += len(data['acts'][j]['scenes'][k]['action'][l]['says'])
-print(cnt(counter_dict))
+                       cnt[data['acts'][j]['scenes'][k]['action'][l]['character']] += len(data['acts'][j]['scenes'][k]['action'][l]['says'])
+print(cnt)
+
+
+
+
