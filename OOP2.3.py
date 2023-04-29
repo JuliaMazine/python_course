0cec82aae8994c6be251d58106c430b57d516af5
Julia Mazine <d.mazine@g.nsu.ru>
2023-04-29 10:57:58 +0700

    added a hierarchy

--- OOP2.3.py
+++ OOP2.3.py
@@ -1,21 +1,22 @@
 class Employee:
-    def __init__(self, name, position, salary):
+    def __init__(self, name, position, salary, hierarchy):
         self.name = name
         self.position = position
         self.salary = salary
+        self.hierarchy = hierarchy
 
     def print_info(self):
-        print(f'Name: {self.name}, Position: {self.position}, Salary: {self.salary}')
+        print(f'Name: {self.name}, Position: {self.position}, Salary: {self.salary}, Position: {self.hierarchy}')
 
 
 class Manager(Employee):
     def __init__(self, name, salary):
-        super().__init__(name, 'Manager', salary)
+        super().__init__(name, 'Manager', salary, "Upper")
 
 
 class Engineer(Employee):
     def __init__(self, name, salary, programming_language):
-        super().__init__(name, 'Engineer', salary)
+        super().__init__(name, 'Engineer', salary, "Middle")
         self.programming_language = programming_language
 
     def print_info(self):
@@ -25,7 +26,7 @@ class Engineer(Employee):
 
 class Salesman(Employee):
     def __init__(self, name, salary, sales_volume):
-        super().__init__(name, 'Salesman', salary)
+        super().__init__(name, 'Salesman', salary, "Middle")
         self.sales_volume = sales_volume
 
     def print_info(self):
@@ -35,7 +36,7 @@ class Salesman(Employee):
 
 class Designer(Employee):
     def __init__(self, name, salary, design_tools):
-        super().__init__(name, 'Designer', salary)
+        super().__init__(name, 'Designer', salary, "Lower")
         self.design_tools = design_tools
 
     def print_info(self):
@@ -45,7 +46,7 @@ class Designer(Employee):
 
 class Accountant(Employee):
     def __init__(self, name, salary, certification):
-        super().__init__(name, 'Accountant', salary)
+        super().__init__(name, 'Accountant', salary, "Middle")
         self.certification = certification
 
     def print_info(self):
@@ -55,13 +56,12 @@ class Accountant(Employee):
 
 class HumanResources(Employee):
     def __init__(self, name, salary, experience_years):
-        super().__init__(name, 'Human Resources', salary)
+        super().__init__(name, 'Human Resources', salary, "Lower")
         self.experience_years = experience_years
 
     def print_info(self):
         super().print_info()
         print(f'Experience Years: {self.experience_years}')
-
 
 
 manager = Manager('John Smith', 150000)
